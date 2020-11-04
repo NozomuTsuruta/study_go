7c61cb8cd4c72a5c5679c4af2d9a4c7c2c86ef87
NozomuTsuruta <tuno.0806@icloud.com>
2020-11-04 23:19:30 +0900

    コメント追加

--- src/study_2/util.go
+++ src/study_2/util.go
@@ -6,8 +6,8 @@ import (
 )
 
 func session(w http.ResponseWriter, r *http.Request) (sess data.Session, err error) {
-	cookie, err := r.Cookie("_cookie")
-	if err == nil {
+	cookie, err := r.Cookie("_cookie") // Cookieを取り出す
+	if err == nil {                    // ログイン状態
 		sess = data.Session{Uuid: cookie.Value}
 		if ok, _ := sess.Check(); !ok {
 			err = errors.New("Invalid session")
