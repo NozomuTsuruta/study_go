4fe727eedfda8959f464cf53022aeb00709a3d26
NozomuTsuruta <tuno.0806@icloud.com>
2020-11-05 23:27:05 +0900

    indexのリファクタリング

--- src/study_2/route_main.go
+++ src/study_2/route_main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"html/template"
 	"net/http"
 )
 
@@ -10,15 +9,10 @@ func index(w http.ResponseWriter, r *http.Request) {
 	threads, err := data.Threads()
 	if err == nil {
 		_, err := session(w, r) // 不要な情報はブランク識別子を使う
-		publicTmplFiles := []string{"templates/layout.html", "templates/public.navbar.html", "templates/index.html"}
-		privateTmplFiles := []string{"templates/layout.html", "templates/public.navbar.html", "templates/index.html"}
-
-		var templates *template.Template
 		if err != nil {
-			// ParseFiles→テンプレートファイルを解析してテンプレートを作成、Mustでエラー検知
-			templates = template.Must(template.ParseFiles(publicTmplFiles...))
+			generateHTML(w, threads, "layout", "public.navbar", "index")
 		} else {
-			templates = template.Must(template.ParseFiles(privateTmplFiles...))
+			generateHTML(w, threads, "layout", "private.navbar", "index")
 		}
 		// テンプレートファイルからコンテンツを取り出し、別のデータと組み合わせてHTMLを生成→テンプレートの実行
 		templates.ExecuteTemplate(w, "layout", threads)
