a3015460b8fd40771b4fa45cd83f538720dfdef5
NozomuTsuruta <tuno.0806@icloud.com>
2020-11-17 21:26:21 +0900

    httpとinterface

--- study_6/main.go
+++ study_6/main.go
@@ -1,19 +1,33 @@
 package main
 
 import (
-	"os"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 )
 
-func main()  {
-	resp,err := http.Get("http://google.com")
+type logWritter struct{}
+
+func main() {
+	resp, err := http.Get("http://google.com")
 	if err != nil {
-		fmt.Println("Error:",err)
+		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 
-	bs := make([]byte, 99999)
-	resp.Body.Read(bs)
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+	// io.Copy(os.Stdout,resp.Body)
+
+	lw := logWritter{}
+
+	io.Copy(lw, resp.Body)
+}
+
+func (logWritter) Write(bs []byte) (int, error) {
 	fmt.Println(string(bs))
+	fmt.Println("Just wrote this may bytes:", len(bs))
+	return len(bs), nil
 }
